8b60bc415ec05f2916e9f77cbbbe8c51dee4f901
Thirupathi <thirupathisundar0212@gmail.com>
2024-11-30 02:26:43 +0530

    new commit

--- my_site/settings.py
+++ my_site/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER':'root',
+        'PASSWORD':'Thiru@0212',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
